de9e13003eee9ff3d4111b18329a33e79b1a7e91
Noah <noahmwangi@gmail.com>
2023-03-19 20:42:10 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(drivers, driverName){
+    return drivers.filter(function(drivers){
+        return drivers.toLowerCase() === driverName.toLowerCase()});
+}
+function fuzzyMatch(drivers, queryName) {
+    let totalChar = queryName.length;
+      return drivers.filter(function(driverName){
+      return driverName.slice(0, totalChar) === queryName;
+    });
+}
+function matchName(drivers, name) {
+    return drivers.filter(function(driver){
+        return driver.name === name;
+    });
+}
+      
